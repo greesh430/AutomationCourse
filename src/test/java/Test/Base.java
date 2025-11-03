7fe905fd3d1b02b7f6e17984e11bd3bf592c7c25
greesh430 <greeshgopak95@gmail.com>
2025-11-03 19:15:28 +0530

    Initial Commit

---
+++ src/test/java/Test/Base.java
@@ -0,0 +1,29 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	public WebDriver driver;
+	
+	public void initializeBrowser(){
+		driver=new ChromeDriver();
+		driver.get("https://selenium.qabible.in/");
+		driver.manage().window().maximize();
+	}
+	
+	public void browserCloseAndQuit() {
+		//driver.close();
+		driver.quit();
+	}
+
+	public static void main(String[] args) {
+		
+		Base base=new Base();
+		base.initializeBrowser();
+		base.browserCloseAndQuit();
+		
+
+	}
+
+}
