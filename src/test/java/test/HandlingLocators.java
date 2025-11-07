862a9b622087c8c308634a9bfac303038facb752
greesh430 <greeshgopak95@gmail.com>
2025-11-07 18:01:38 +0530

    Committing Handling Locators and Web Element Commands

--- src/test/java/test/HandlingLocators.java
+++ src/test/java/test/HandlingLocators.java
@@ -12,6 +12,18 @@ public class HandlingLocators extends Base{
 		driver.findElement(By.name("viewport"));
 		driver.findElement(By.linkText("Ajax Form Submit"));
 		driver.findElement(By.partialLinkText("Ajax"));
+		driver.findElement(By.cssSelector("button[id='button-one']"));
+		driver.findElement(By.xpath("//input[@id='single-input-field']"));
+		driver.findElement(By.xpath("//button[text()='Show Message']"));
+		driver.findElement(By.xpath("//button[@id='button-one' and @type='button']"));
+		driver.findElement(By.xpath("//button[@id='button-one' or @id='button-one-electronics']"));
+		driver.findElement(By.xpath("//div[contains (text(), 'Single Input Field')]//parent: :div[@class='card']"));
+		driver.findElement(By.xpath("//div[@class='card']//child::button[@id= 'button-one']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//following::div[@class='card']"));
+		driver.findElement(By.xpath("//buttonfOid-'button-one' 1//preceding:: div[@class-'card']"));
+		driver.findElement(By.xpath("//button[@id='button-one':]//ancestor::div"));
+		driver.findElement(By.xpath("//div[@class='card']//descendant::div"));
+				
 		
 	}
 
@@ -22,3 +34,6 @@ public class HandlingLocators extends Base{
 	}
 
 }
+
+
+///html/body/section/div/div/div[2]/div[1]/div/div[2]/form/button : absolute XPath
