826a6b1eaa5999485d433f20af75e636ce3390d7
greesh430 <greeshgopak95@gmail.com>
2025-11-13 22:06:23 +0530

    Committing handling actions

---
+++ src/test/java/test/HandlingActions.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class HandlingActions extends Base{
+	public void verifyRightClick() {
+		WebElement others = driver.findElement(By.id("others"));
+		Actions actions = new Actions(driver);
+		actions.contextClick().build().perform();	
+	}
+	public void verifyMouseHover() {
+		WebElement others = driver.findElement(By.id("others"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(others).build().perform();
+	}
+	public void verifyDragandDrop() {
+		driver.navigate().to("https://demoqa.com/droppable");
+		WebElement dragme = driver.findElement(By.id("draggable"));
+		WebElement drop = driver.findElement(By.id("droppable"));
+		Actions actions = new Actions(driver);
+		actions.dragAndDrop(dragme, drop).build().perform();
+	}
+	public void verifyKeyboardAction() throws AWTException {
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_T);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_T);
+	}
+	public static void main(String[] args) {
+		HandlingActions ha = new HandlingActions();
+		ha.initializeBrowser();
+		//ha.verifyRightClick();
+		//ha.verifyMouseHover();
+		//ha.verifyDragandDrop();
+		try {
+			ha.verifyKeyboardAction();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
