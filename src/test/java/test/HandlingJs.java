15f7cf445e67e4850b549573c9b6fb6457414b03
greesh430 <greeshgopak95@gmail.com>
2025-11-19 18:09:23 +0530

    Committing handlingjs & added testng dependencies in pom.xml

---
+++ src/test/java/test/HandlingJs.java
@@ -0,0 +1,27 @@
+package test;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class HandlingJs extends Base{
+public void verfyJsExecutor() {
+		
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		
+		WebElement showMessageButton=driver.findElement(By.xpath("//button[@id='button-one']"));
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", showMessageButton);
+		
+		js.executeScript("window.scrollBy(0,350)", ""); //scroll down
+		//js.executeScript("window.scrollBy(0,-350)", ""); //scroll up
+}
+	public static void main(String[] args) {
+		HandlingJs handlingJs=new HandlingJs();
+		handlingJs.initializeBrowser();
+		handlingJs.verfyJsExecutor();
+	}
+
+}
