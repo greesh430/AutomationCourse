ca4527438543a67cfede5ee8712a0d1754f7c9ea
greesh430 <greeshgopak95@gmail.com>
2025-12-14 12:00:04 +0530

    Committing Base TestNG

---
+++ src/test/java/testscripts/Base.java
@@ -0,0 +1,24 @@
+package testscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base {
+	
+	public WebDriver driver;
+	@BeforeMethod
+	public void initializeBrowser(){
+		driver=new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+	}
+	
+	@AfterMethod
+	public void browserCloseAndQuit() {
+		//driver.close();
+		//driver.quit();
+	}
+
+}
